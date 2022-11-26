a97257864584cf3814e22321ec9bb9ec89a5676f
erina <erinafausett@gmail.com>
2022-11-26 16:36:33 +0900

    質問アコーディオンメニュー作成中

--- src/views/pages/Helpre.js
+++ src/views/pages/Helpre.js
@@ -1,19 +1,69 @@
+import {BsPlusLg} from 'react-icons/bs'
+import { IconContext } from 'react-icons'
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Heading from '../components/Heading';
 import Subtitle from '../components/Subtitle';
 import UpArrow from '../components/UpArrow';
 
+import FlexLink from "src/util/FlexLink";
 
+import React, {useState} from 'react';
 
+import HelperStyle from '../../style/pages/Helpre.module.css';
 const Helper = () => {
+  const [open, setOpen] = useState(true);
+  const toggle = () => setOpen(!open);
+  const QuestionList=[
+    {question:"面接の流れを教えてください。", answer:<>まずは指定のGoogleフォームで開発経験や希望する班をお聞きします。
+                                        回答を送信するとDiscordへの招待リンクが届きますので入室してください。
+                                        入室後 #面接の流れチャンネルをご確認の上、
+                                        #面接日時決定チャンネルで面接希望日時を複数記入いただきます。
+                                        面接は30分程度を予定しております。(複数班希望されると長くなりやすいです。)
+                                        面接終了後、面接官同士で審議を行い合否通知をDiscord上でお出しします。
+                                        ※Googleフォームも上記の記事からご記入いただけます。</>},
+    {}
+  ]
   return (
     <div>
       <Header/>
       <main>
         <Heading heading="運営応募"/>
         <Subtitle subtitle="運営って何？"/>
+          <div className='subtitle-content'>
+            <p>
+              運営とは、畳サーバーのイベント開発やマップ製作、テクスチャ作成、動画編集等様々な分野でお手伝いをいただいている方の総称です。<br/>
+              もちろん全てを出来なければならないわけではありませんし、足りない技術は覚えながら補えば構いません。<br/>
+              <FlexLink href="https://mineidea.net/projects/6160894485">
+              詳細はこちらから！
+              </FlexLink>
+            </p>
+          </div>
         <Subtitle subtitle="よくある質問"/>
+        <div className={HelperStyle["Question-wrapper"]}>
+        {QuestionList.map(({question, answer}) => {
+            return (
+              <div className={HelperStyle["QandA-container"]}>
+                <button onClick={toggle}>{open? 'close':'open'}</button>
+                  <div className={HelperStyle["question-container"]}>
+                    <div className={HelperStyle["q-icon"]}>Q</div>
+                    <p className={HelperStyle.question}>{question}</p>
+                    <IconContext.Provider value={{ color: '#436644', size: '20px' }}>
+                      <BsPlusLg className={`${HelperStyle["plus-icon"]} ${HelperStyle.rotate}`}/>
+                    </IconContext.Provider>
+                </div>
+                <div className={open? 'isOpen':'isClose'} >
+                  <div className={HelperStyle["answer-container"]}>
+                  <div className={HelperStyle["a-icon"]}>A</div>
+                  <p className={HelperStyle.answer}>{answer}</p>
+                  </div>
+                </div>
+              </div>
+              );
+            })}
+        </div>
+
         <UpArrow/>
       </main>
       <Footer/>
--- src/views/pages/Subscription.js
+++ src/views/pages/Subscription.js
@@ -22,33 +22,66 @@ const Subscription = () => {
     {title:"限定企画に参加できる！", img:sub1, description:"村人防衛戦・スコットランドヤード・人数制限のある配布マップ等の限定企画に参加できます！"},
     {title:"通常イベントも優先参加可能！", img:sub2, description:"人狼のような参加人数制限があるイベントについても、より優先的にゲームに参加できます。"},
     {title:"Twitchで広告がつかない！", img:sub3, description:"Twitchの生配信を広告なしで視聴できるようになります。快適です!"},
-    {title:"チャネルポイント「Takika」が貯まりやすい！", img:sub4, description:"Twitchの視聴や継続ログインで得られるチャンネルポイント「Takika」が貯まりやすくなります!貯めたポイントは、ゲームリクエストや企画提案、歌枠での曲のリクエスト等に使うことができます。"},
+    {title:"チャネルポイント「Takika」が貯まりやすい！", img:sub4, description:<>Twitchの視聴や継続ログインで得られるチャンネルポイント「Takika」が貯まりやすくなります!<br/>貯めたポイントは、ゲームリクエストや企画提案、歌枠での曲のリクエスト等に使うことができます。</>},
     {title:"専用スタンプが使用可能に！", img:sub5, description:"Twitch及びDiscordでオリジナルスタンプが利用可能になります!配信や会話を盛り上げよう！"},
-    {title:"お名前の横にサブスクライバーバッジが付く", img:sub6, description:"サブスクライバーの証となるバッジが付与され、他の方と違った雰囲気を出すことが可能です。しかもこのバッジは、サブスク年月によって成長していきます。"},
-    {title:"付近vcMODが使用できる（Java版限定）", img:sub7, description:"サブスクライバー限定で、付近vcMODでの通話ができるようになりました!味方との連携をする際や敵の位置を察知する際など、楽しさの幅がぐっと広がります！"},
-  ]
+    {title:"お名前の横にサブスクライバーバッジが付く", img:sub6, description:<>サブスクライバーの証となるバッジが付与され、他の方と違った雰囲気を出すことが可能です。<br/>しかもこのバッジは、サブスク年月によって成長していきます。</>},
+    {title:"付近vcMODが使用できる（Java版限定）", img:sub7, description:<>サブスクライバー限定で、付近vcMODでの通話ができるようになりました!<br/>味方との連携をする際や敵の位置を察知する際など、楽しさの幅がぐっと広がります！</>},
+  ];
+  const SpecialThenksList=["Yukina様", "S2ns_mash様", "まりゅ🐴様", "虚空教信者様"] 
   return (
     <div>
       <Header/>
       <main>
         <Heading heading="サブスクライブ・ご寄付について"/>
         <Subtitle subtitle="サブスクライバーとは？"/>
+        <div className='subtitle-content'>
           <img className='subtitle-img' src={twitchImg} alt="twitchの画像"></img>
+        </div>
         <Subtitle subtitle="サブスクライブのメリット！"/>
-        <div className={SubscriptionStyle["LargeCategory-wrapper"]}>
-          {LargeCategoryContent.map(( {title, img, description }) => {
-            return (
-              <div className={SubscriptionStyle["LargeCategory-container"]}>
-                <LargeCategory LargeCategory={title}/>
-                <div className={SubscriptionStyle["LargeCategory-content"]}>
-                  <img src={img}></img>
-                  <p>{description}</p>
+        <div className='subtitle-content'>
+          <div className={SubscriptionStyle["LargeCategory-wrapper"]}>
+            {LargeCategoryContent.map(( {title, img, description }) => {
+              return (
+                <div className={SubscriptionStyle["LargeCategory-container"]}>
+                  <LargeCategory LargeCategory={title}/>
+                  <div className={SubscriptionStyle["LargeCategory-content"]}>
+                    <img src={img}></img>
+                    <p>{description}</p>
+                  </div>
                 </div>
-              </div>
+                );
+              })}
+          </div>
+        </div>
+        <Subtitle subtitle="ご寄付"/>
+        <div className='subtitle-content'>
+          <img src={doneruLogo}></img>
+          <p>
+            平素より、畳サーバー並びに鯖主:おとたきの活動に関心をお寄せくださり、
+            誠にありがとうございます。<br/>
+            この度、サーバー維持の資金として、ご寄付を募ることに致しました。
+          </p>
+          <ul>
+            <li>[使用実績/使用予定]</li>
+            <li>Minecraft有料サーバーのレンタル</li>
+            <li>配信機材　静音キーボード　オーディオインターフェース等の購入</li>
+            <li>新規ゲームやシステムの開発</li>
+          </ul>
+          <p>
+            PayPalまたはクレジットカードでのご寄付が可能です。<br/>
+            ご寄付くださった方は、Special Thanksにて氏名(またはニックネーム)を掲載させていただきます。
+          </p>
+          <p className={SubscriptionStyle["special-thenks"]}>Special Thenks</p>
+          <table>
+          {SpecialThenksList.map((name) => {
+            return (
+              <tr>
+                <td>{name}</td>
+              </tr>
               );
             })}
-          </div>
-        <Subtitle subtitle="ご寄付"/>
+          </table>
+        </div>
         <UpArrow/>
       </main>
       <Footer/>
