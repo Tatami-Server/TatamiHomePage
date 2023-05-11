4068fe12896980b8b50848cdcf0085ed7f2e57fc
erina <erinafausett@gmail.com>
2023-05-11 16:21:08 +0900

    ハンバーガーメニューデザイン完成

--- src/views/components/Menu.js
+++ src/views/components/Menu.js
@@ -3,21 +3,30 @@ import { stack as Burger } from 'react-burger-menu';
 // import { FlexLink } from "react-router-dom";
 import FlexLink from '../../util/FlexLink';
 
-export default props =>{
+export default (props) => {
+  const links = [
+    { href: "/", text: "Home" },
+    { href: "/join/", text: "サーバー参加方法" },
+    { href: "/event/", text: "イベント一覧" },
+    { href: "/map/", text: "配布マップ" },
+    { href: "/life/", text: "生活サーバー" },
+    { href: "/hishrefry/", text: "畳サーバーの歴史" },
+    { href: "/please/", text: "運営からのお願い" },
+    { href: "/sbuscription/", text: "サブスクライブ・ご寄付" },
+    { href: "/sns/", text: "公式SNS等" },
+    { href: "/contact/", text: "お問い合わせ・意見箱" },
+    { href: "/helpre/", text: "運営への応募" },
+  ];
 
   return (
-    <Burger{...props}>
-      <FlexLink href="/">Home</FlexLink>
-      <FlexLink href="/join/">サーバー参加方法</FlexLink>
-      <FlexLink href="/event/">イベント一覧</FlexLink>
-      <FlexLink href="/map/">配布マップ</FlexLink>
-      <FlexLink href="/life/">生活サーバー</FlexLink>
-      <FlexLink href="/hishrefry/">畳サーバーの歴史</FlexLink>
-      <FlexLink href="/please/">運営からのお願い</FlexLink>
-      <FlexLink href="/sbuscription/">サブスクライブ・ご寄付</FlexLink>
-      <FlexLink href="/sns/">公式SNS等</FlexLink>
-      <FlexLink href="/contact/">お問い合わせ・意見箱</FlexLink>
-      <FlexLink href="/helpre/">運営への応募</FlexLink>
+    <Burger {...props}>
+      <ul>
+        {links.map((link, index) => (
+          <li key={index}>
+            <FlexLink href={link.href}>{link.text}</FlexLink>
+          </li>
+        ))}
+      </ul>
     </Burger>
   );
 };
