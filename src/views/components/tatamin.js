342b94ecdc961455d5cf21d801ef7f9cb95e65c1
erina <erinafausett@gmail.com>
2023-05-14 18:59:46 +0900

    tataminアニメーション追加

--- src/views/components/tatamin.js
+++ src/views/components/tatamin.js
@@ -7,26 +7,25 @@ import tataminSister from '../../images/tatamin.images/sister2.png';
 import tatamiMaru from '../../images/tatamin.images/tatamimaru.png';
 
 const Tatamin = () => {
-  const [showTatamin, setTatamin] = useState(true);
+  const [showTatamin, setTatamin] = useState(false);
 
   function handleClick() {
     setTatamin(!showTatamin);
   }
   return (
-    <div>
-      <div className={Style['tatamin-container']} onClick={handleClick}>
-      {showTatamin ? 
-        <img className={Style['tatamin-maru']} src={tatamiMaru} alt="Image" /> : 
-        <div className={Style['tatamin']}>
-          <img className={Style['tatamin-img']} src={tatamin} alt="Image" />
-          <div  className={Style['tatamin-text-container']}>
-            <p>こんにちは！私はたたみんです。</p>
+    <>
+      <img className={`${Style['tatamin-maru']} ${!showTatamin ? Style['show']: Style['visible'] }`} src={tatamiMaru} alt="Image" onClick={handleClick} />
+      <div className={`${Style['tatamin-wrapper']} ${showTatamin ? Style['show']: Style['visible']}`} onClick={handleClick}>
+        <div className={Style['tatamin-container']}>
+          <div className={Style['tatamin']}>
+            <img className={Style['tatamin-img']} src={tatamin} alt="Image" />
+            <div  className={Style['tatamin-text-container']}>
+              <p>こんにちは！私はたたみんです。</p>
+            </div>
           </div>
-          </div>
-        }
+        </div>
       </div>
-
-    </div>
+    </>
   );
 };
 
