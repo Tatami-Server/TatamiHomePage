2fdf38383a35c377e56387cfb85b0408b80ce54b
erina <erinafausett@gmail.com>
2023-05-14 16:55:20 +0900

    たたみんをクリックしたら画像が変更される機能作成

--- src/views/components/tatamin.js
+++ src/views/components/tatamin.js
@@ -1,15 +1,29 @@
-import Style from '../../style/components/tatamin.module.css';
+import React, { useState } from 'react';
+
+import Style from '../../style/components/Tatamin.module.css';
 
 import tatamin from '../../images/tatamin.images/sister1.png';
 import tataminSister from '../../images/tatamin.images/sister2.png';
 import tatamiMaru from '../../images/tatamin.images/tatamimaru.png';
 
 const Tatamin = () => {
+  const [showTatamin, setTatamin] = useState(true);
 
+  function handleClick() {
+    setTatamin(!showTatamin);
+  }
   return (
     <div>
-      <div className={Style['tatami-maru']}>
-        <img src={tatamiMaru} />
+      <div className={Style['tatamin-container']} onClick={handleClick}>
+      {showTatamin ? 
+        <img className={Style['tatamin-maru']} src={tatamiMaru} alt="Image" /> : 
+        <div className={Style['tatamin']}>
+          <img className={Style['tatamin-img']} src={tatamin} alt="Image" />
+          <div  className={Style['tatamin-text-container']}>
+            <p>こんにちは！私はたたみんです。</p>
+          </div>
+          </div>
+        }
       </div>
 
     </div>
