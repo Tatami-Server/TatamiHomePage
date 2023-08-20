3596a87a383f698039353a44f5a9350569db1dbe
Hiroya In <hiroya.in@flagship.cc>
2023-08-21 01:21:20 +0900

    管理画面の移行

--- nextjs/src/pages/_app.js
+++ nextjs/src/pages/_app.js
@@ -1,9 +1,10 @@
 import MainLayout from "@layouts/MainLayout";
+import NoLayout from "@layouts/NoLayout";
 
 import '@style/common.css'
 
-function MyApp({ Component, pageProps }) {
-    const Layout = Component.Layout || MainLayout;
+function MyApp({ Component, pageProps, router }) {
+    const Layout = router.pathname.includes('admin') ? NoLayout : Component.Layout || MainLayout;
     return (
         <Layout>
             <Component {...pageProps} />
