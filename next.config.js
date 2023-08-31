c720541bb6b59baa214a285cf873001fcd39d6f1
Hiroya In <hiroya.in@flagship.cc>
2023-08-31 22:23:39 +0900

    許可ドメインの追加

--- next.config.js
+++ next.config.js
@@ -2,14 +2,11 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'mineskin.eu',
-            }
+            { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+            { protocol: 'https', hostname: 'mineskin.eu' },
+            { protocol: 'https', hostname: 'tatami-f2ad1.firebaseapp.com' },
+            { protocol: 'https', hostname: 'dev-tatami.web.app' },
+            { protocol: 'https', hostname: 'tatamiserver.com' }
         ]
     }
 }
