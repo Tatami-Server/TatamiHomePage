aa8a1fa2bcf9faaeda13fba945253c20332eb116
Hiroya In <hiroya.in@flagship.cc>
2023-09-01 17:58:13 +0900

    firebase hosting使用廃止　不要ファイル整理

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first")
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        unoptimized: false,
         remotePatterns: [
             { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
             { protocol: 'https', hostname: 'mineskin.eu' },
-            { protocol: 'https', hostname: 'tatami-f2ad1.firebaseapp.com' },
-            { protocol: 'https', hostname: 'dev-tatami.web.app' },
-            { protocol: 'https', hostname: 'tatamiserver.com' },
         ]
     }
 }
