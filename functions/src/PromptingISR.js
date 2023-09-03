2f357640b582d70857f5328d62ab65870b824f50
rusian0 <hiroya.in@flagship.cc>
2023-09-03 16:08:38 +0900

    ロガー追加

--- functions/src/PromptingISR.js
+++ functions/src/PromptingISR.js
@@ -6,19 +6,21 @@ const topCollections = ['top', 'notice']
 module.exports = firestore.onDocumentWritten('{collection}/{docId}', async (event) => {
     const { collection, docId } = event.params
     const baseUrl = process.env.BASE_URL
-
+    logger.log(event.params)
+    
     const isTop = topCollections.some(colName => collection.includes(colName));
-
+    
     const pathName = isTop ? '' : collection
     const collectionUrl = `${baseUrl}/${pathName}`
-
+    
     const urls = [collectionUrl]
     if (!isTop) urls.push(`${collectionUrl}/${docId}`)
-
+    
+    logger.log(urls)
     for (const url of urls) {
         try {
             await got(url)
-            console.log('success')
+            logger.log('success')
         } catch (error) {
             logger.log('not found')
         }
