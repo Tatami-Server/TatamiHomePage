b023008ff4644d326d60b05a3b8b5dda5b7bdd7b
erina <erinafausett@gmail.com>
2023-09-28 23:46:44 +0900

    thanksページ追加

---
+++ src/pages/contact/thanks.js
@@ -0,0 +1,26 @@
+
+// コンポーネントインポート
+import Heading from '@components/Heading';
+
+// cssインポート
+import Style from '@style/pages/Contact.module.scss';
+import { Link } from 'react-router-dom';
+
+const Thanks = () => {
+
+
+  return (
+    <div>
+      <Heading heading="お問い合わせ・意見箱" />
+      <div className={Style["thanks-card"]}>
+        <div className={Style["card-text"]}>
+          <p>お問い合わせありがとうございます。<br />送信完了しました。</p>
+          <Link href={'/'}>
+            <button>ホームに戻る</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Thanks;
