e49e88c9f503dd84cfbba493400c7f5f9ce83d87
rusian0 <hiroya.in@flagship.cc>
2023-09-30 00:01:54 +0900

    レスポンス変更

--- src/pages/api/getMinecraftServerStat.js
+++ src/pages/api/getMinecraftServerStat.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
                 const {
                     onlinePlayers
                 } = stat
-                
+
                 const statData = {
                     type,
                     players: Object.keys(onlinePlayers.players)
@@ -25,7 +25,6 @@ export default async function handler(req, res) {
             res.status(200).json(serverStats)
         })
         .catch((error) => {
-            console.error('エラーが発生しました:', error);
-            res.status(500).json({error})
+            res.status(200).json([])
         });
 };
